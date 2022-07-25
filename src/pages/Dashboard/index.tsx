c3f97c091992ca95b44301818c9c7bec83a89cbf
Rob Park <robpark04@gmail.com>
2022-07-25 13:45:03 -0400

    feat: main function

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,11 +1,42 @@
-import React, { useState, useEffect } from 'react';
-import { Row, Col, Button, ButtonGroup, Card } from 'react-bootstrap';
-
+import React from 'react';
+import Web3 from 'web3';
+import { Row, Col, Button, ButtonGroup, Card, Modal} from 'react-bootstrap';
+import Form from 'react-bootstrap/Form';
+import { useState, useEffect } from 'react';
 import './index.scss';
+import cardLogo from '../assets/img/card-logo.png';
 
+import { useDispatch, useSelector } from "react-redux";
+import * as selectors from "store/selectors";
+import { updateWalletConnection } from "store/actions";
+import { tokenAddresses } from 'config';
+import { AbiItem } from 'web3-utils';
+import tokenABI from '../../abi/token.abi.json';
+import stakingABI from '../../abi/staking.abi.json';
+import { StakingAddress } from 'config';
 
-const Casinoswap = () => {
+declare let window: any;
 
+const Dashboard = () => {
+
+    const dispatch = useDispatch();
+    const WalletState = useSelector(selectors.WalleteState);
+    const connectionState = WalletState.wallet_connection;
+    const wallet_balance = WalletState.wallet_balance;
+    const web3 = new Web3(window.ethereum);
+
+    const contract = new web3.eth.Contract(stakingABI as AbiItem[], StakingAddress);
+    const onClaim = async() =>{
+        await contract.methods.claimReward().send({
+            from : WalletState.account_address
+        });
+    };
+
+    const onCompound = async() => {
+        await contract.methods.compound(WalletState.account_address).send({
+            from : WalletState.account_address
+        });
+    };
     return (
         <div className="home-container mb-5" style={{ fontFamily: 'Inter', color: 'white'}}>
             <Row style={{justifyContent:'center', marginTop:"40px"}}>
@@ -24,8 +55,8 @@ const Casinoswap = () => {
                                 <div className="daboard-content-header">Earned RDX</div>
                                 <div className='daboard-content-body'>12.30</div>
                                 <div className='button-layout'>
-                                    <Button className='dashboard-content-button'>Claim</Button>
-                                    <Button className='dashboard-content-button-compound'>Compound</Button>
+                                    <Button className='dashboard-content-button' onClick={() => onClaim()}>Claim</Button>
+                                    <Button className='dashboard-content-button-compound' onClick={() => onCompound()}>Compound</Button>
                                 </div>
                             </Col>
                             <Col xl="3" md="6" sm="6" xs="6" style={{padding:"0"}}>
@@ -45,4 +76,4 @@ const Casinoswap = () => {
     );
 };
 
-export default Casinoswap;
+export default Dashboard;
